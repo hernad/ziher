1ea806e5b9d4053bd1f495c063506850223507d7
hernad <hernad@bring.out.ba>
2020-02-17 11:10:27 +0100

    De-Clipper

---
+++ src/zh_rtl/rdd/db_base_func_trans.c
@@ -0,0 +1,91 @@
+/*
+ * Internal versions for database version
+ *
+ * Copyright 1999-2001 Viktor Szakats (vszakats.net/ziher)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file LICENSE.txt.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA (or visit https://www.gnu.org/licenses/).
+ *
+ * As a special exception, the Ziher Project gives permission for
+ * additional uses of the text contained in its release of Ziher.
+ *
+ * The exception is that, if you link the Ziher libraries with other
+ * files to produce an executable, this does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * Your use of that executable is in no way restricted on account of
+ * linking the Ziher library code into it.
+ *
+ * This exception does not however invalidate any other reasons why
+ * the executable file might be covered by the GNU General Public License.
+ *
+ * This exception applies only to the code released by the Ziher
+ * Project under the name Ziher.  If you copy code from other
+ * Ziher Project or Free Software Foundation releases into a copy of
+ * Ziher, as the General Public License permits, the exception does
+ * not apply to the code that you add in this way.  To avoid misleading
+ * anyone as to the status of such modified files, you must delete
+ * this exception notice from them.
+ *
+ * If you write modifications of your own for Ziher, it is your choice
+ * whether to permit this exception to apply to your modifications.
+ * If you do not wish that, delete this exception notice.
+ *
+ */
+
+#include "zh_defs.h"
+
+
+ZH_FUNC_TRANSLATE( __DBSEEK, DBSEEK )
+ZH_FUNC_TRANSLATE( __DBSKIP, DBSKIP )
+ZH_FUNC_TRANSLATE( __DBGOTOP, DBGOTOP )
+ZH_FUNC_TRANSLATE( __DBGOBOTTOM, DBGOBOTTOM )
+ZH_FUNC_TRANSLATE( __DBGOTO, DBGOTO )
+ZH_FUNC_TRANSLATE( __DBAPPEND, DBAPPEND )
+ZH_FUNC_TRANSLATE( __DBDELETE, DBDELETE )
+ZH_FUNC_TRANSLATE( __DBRECALL, DBRECALL )
+ZH_FUNC_TRANSLATE( __DBCOMMITALL, DBCOMMITALL )
+ZH_FUNC_TRANSLATE( __DBUNLOCK, DBUNLOCK )
+ZH_FUNC_TRANSLATE( __DBUNLALL, DBUNLOCKALL )
+ZH_FUNC_TRANSLATE( __DBSETFILTER, DBSETFILTER )
+ZH_FUNC_TRANSLATE( __DBCLEARRELATION, DBCLEARRELATION )
+ZH_FUNC_TRANSLATE( __DBSETRELATION, DBSETRELATION )
+ZH_FUNC_TRANSLATE( __DBREINDEX, DBREINDEX )
+ZH_FUNC_TRANSLATE( __DBCREATINDEX, DBCREATEINDEX )
+ZH_FUNC_TRANSLATE( __DBCLEARINDEX, DBCLEARINDEX )
+ZH_FUNC_TRANSLATE( __DBSETINDEX, DBSETINDEX )
+ZH_FUNC_TRANSLATE( __DBSETORDER, DBSETORDER )
+ZH_FUNC_TRANSLATE( __DBCLOSEAREA, DBCLOSEALL )
+ZH_FUNC_TRANSLATE( __DBCLOSE, DBCLOSEAREA )
+ZH_FUNC_TRANSLATE( __DBUSE, DBUSEAREA )
+ZH_FUNC_TRANSLATE( __DBSELECT, DBSELECTAREA )
+ZH_FUNC_TRANSLATE( ZH_DBPACK, __DBPACK )
+ZH_FUNC_TRANSLATE( ZH_DBZAP, __DBZAP )
+ZH_FUNC_TRANSLATE( DBEXISTS, ZH_DBEXISTS )
+ZH_FUNC_TRANSLATE( DBDROP, ZH_DBDROP )
+ZH_FUNC_TRANSLATE( DBRENAME, ZH_DBRENAME )
+ZH_FUNC_TRANSLATE( FIELDLEN, ZH_FIELDLEN )
+ZH_FUNC_TRANSLATE( FIELDDEC, ZH_FIELDDEC )
+ZH_FUNC_TRANSLATE( FIELDTYPE, ZH_FIELDTYPE )
+
+ZH_FUNC_TRANSLATE( RDDINFO, ZH_RDDINFO )
+
+ZH_FUNC_TRANSLATE( DBSETINDEX   , ORDLISTADD )
+ZH_FUNC_TRANSLATE( DBCLEARINDEX , ORDLISTCLEAR )
+ZH_FUNC_TRANSLATE( DBREINDEX    , ORDLISTREBUILD )
+ZH_FUNC_TRANSLATE( INDEXEXT     , ORDBAGEXT )
+
+
+
+
