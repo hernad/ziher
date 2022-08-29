966e73fcd7cda9b7e7339f3a0c5b91886bfba197
Ernad Husremovic <hernad@bring.out.ba>
2022-08-29 09:11:56 +0200

    gtwvt: bez -DGT_DEFAULT_CONSOLE

--- src/run_py_f18.cmd
+++ src/run_py_f18.cmd
@@ -2,6 +2,9 @@ cd C:\dev\ziher\ziher_mono\ziher\src\bazel-bin\test
 
 copy /Y f18ziherpy.dll f18ziherpy.pyd
 
-python -c "callback = lambda x,y: x+y+3;import f18ziherpy;f18ziherpy.vminit();f18ziherpy.run('MAIN',1,1);quit()"
+python -c "callback = lambda x,y: x+y+3;import f18ziherpy;f18ziherpy.vminit();f18ziherpy.run('MAIN0',1,0);f18ziherpy.run('MAIN',0,1);quit()"
+
+
+REM python -c "callback = lambda x,y: x+y+3;import f18ziherpy;f18ziherpy.vminit();f18ziherpy.run('MAIN',1,1);quit()"
 
 cd C:\dev\ziher\ziher_mono\ziher\src
