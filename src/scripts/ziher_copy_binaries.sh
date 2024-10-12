e6a574d6ef2633dac84c19c8e2b36a444d1971eb
Ernad Husremovic <hernad@bring.out.ba>
2024-10-12 11:56:31 +0200

    .so

--- src/scripts/ziher_copy_binaries.sh
+++ src/scripts/ziher_copy_binaries.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+chmod +w $ZIHER_HOME/bin/*
+chmod +w $ZIHER_HOME/lib/*
+
 cp -av bazel-bin/zh_comp/main/zhcomp $ZIHER_HOME/bin
 
 cp -av bazel-bin/F18/F18-klijent-lib.so $ZIHER_HOME/lib/f18klijentlib.so
