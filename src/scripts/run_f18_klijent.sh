a6d0825b00ba95ccbb2984031d0c961080b7a324
Ernad Husremovic <hernad@bring.out.ba>
2024-10-12 12:28:36 +0200

    scripts/copy_binaries.sh

--- src/scripts/run_f18_klijent.sh
+++ src/scripts/run_f18_klijent.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+ZH_SRC=/home/hernad/ziher/ziher_src/src
+
+ZH_DEBUG_PATH="$ZH_SRC/F18"
+
+for p in F18/common F18/core F18/core_dbf F18/core_pdf F18/core_sql F18/core_string F18/fin F18/kalk F18/fakt F18/pos
+do
+  ZH_DEBUG_PATH+=":$ZH_SRC/$p"
+done
+
+
+
+echo "ZH_DEBUG_PATH: $ZH_DEBUG_PATH"
+read
+
 cat <<EOF | python 
 import sys
 import os
